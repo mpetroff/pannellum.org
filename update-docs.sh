3da1b9ccb90380fac80ec8e86a5ef5d5efa24f80
Matthew Petroff <matthew@mpetroff.net>
2019-08-21 10:10:57 -0400

    Update for Pannellum 2.5.3.

--- update-docs.sh
+++ update-docs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Generates documentation files.
-# Requires documentationjs <http://documentation.js.org/>.
+# Requires documentationjs <https://documentation.js.org/>.
 
 echo "Generating documentation..."
 
 echo "---\ntitle: API\ntype: api\n---\n" > content/documentation/api/index.md
-documentation build pannellum/src/js/pannellum.js pannellum/src/js/libpannellum.js -f md --name Pannellum --project-version `cat pannellum/VERSION` >> content/documentation/api/index.md
+documentation build pannellum/src/js/pannellum.js pannellum/src/js/libpannellum.js -f md --project-name Pannellum --project-version `cat pannellum/VERSION` --markdown-toc false >> content/documentation/api/index.md
 
 echo "---\ntitle: Reference\ntype: reference\n---\n" > content/documentation/reference/index.md
 cat pannellum/doc/url-config-parameters.md >> content/documentation/reference/index.md
